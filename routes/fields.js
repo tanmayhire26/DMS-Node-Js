0bb9c9976b9eb05aec76e5fc260c7750a1887030
tanmayhire26 <tanmayhire26@yahoo.com>
2022-10-25 00:33:48 +0530

    made req.body.name for field name input an other minor changes for fields amd doctypefields ui crud

--- routes/fields.js
+++ routes/fields.js
@@ -18,12 +18,38 @@ router.post("/", auth, async (req, res) => {
 	const { error } = validateFields(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
 
-	const field = await new Field({
+	const field = new Field({
 		name: req.body.name,
 	});
 
 	await field.save();
 	res.send(field);
+});
+
+router.put("/:id", auth, async (req, res) => {
+	const { error } = validateFields(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	const field = await Field.findByIdAndUpdate(
+		req.params.id,
+		{
+			$set: {
+				name: req.body.name,
+			},
+		},
+		{ new: true }
+	);
+
+	if (!field) return res.status(404).send("Field not found");
+
+	res.status(200).send(field);
+});
+
+router.delete("/:id", auth, async (req, res) => {
+	const field = await Field.findByIdAndDelete(req.params.id);
+	if (!field) return res.status(404).send("Field not found");
+
+	res.status(200).send(field);
 });
 
 module.exports = router;
